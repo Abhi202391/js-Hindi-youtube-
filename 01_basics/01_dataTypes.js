0a7ec1668f84a1ad394fc0133ba98a620877739e
Abhishek Kumar <141223117+Abhi202391@users.noreply.github.com>
2025-12-11 09:06:35 +0000

    container config done

--- 01_basics/01_dataTypes.js
+++ 01_basics/01_dataTypes.js
@@ -1,3 +1,21 @@
 "use strict";// treat all JS code as newer version 
 
-//alert( 3+ 3) // we are using 
+//alert( 3+ 3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) // code readibilty should be high. These types of writing are Bad practices 
+
+console.log("Hitesh")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// DataTypes are 
+// number => 2 to power 53 for positive number 
+// bigint, for larger number, used in stock market or bigger application like facebook or reddit 
+// string => "" or ''
+// boolean 
+// null: it is dataType as well as standalone vlue too that is null is a type here  
+// undefined 
